633dac2a5a2e0c53a4ebfe1d0cfb70eecd2a2b65
Luis Ortiz <luis.ortiz@millicom.com>
2025-01-24 21:42:35 -0600

    feat: generate button variants

---
+++ lib/colors/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+final class DSColors {
+  static const Color primary = Color(0xFFFFC107);
+  static const Color secondary = Color(0xFFFF7043);
+  static const Color accentSuccess = Color(0xFFCDDC39);
+  static const Color accentError = Color(0xFFF44336);
+  static const Color textPrimary = Color(0xFF333333);
+  static const Color textSecondary = Color(0xFF9E9E9E);
+  static const Color backgroundDisabled = Color(0xFFE0E0E0);
+  static const Color foregroundDisabled = Color(0xFFBDBDBD);
+}
--- lib/components/widgets/button.dart
+++ lib/components/widgets/button.dart
@@ -1,19 +1,83 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_design_system/colors/colors.dart';
+
+enum DSButtonVariant {
+  primary,
+  secondary,
+  tertiary,
+}
 
 class DSButton extends StatelessWidget {
   final String text;
   final VoidCallback? onPressed;
+  final DSButtonVariant variant;
+  final IconData? icon;
   const DSButton({
     required this.text,
     required this.onPressed,
+    this.variant = DSButtonVariant.primary,
+    this.icon,
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    return MaterialButton(
+    return RawMaterialButton(
+      fillColor: _getBackgroundColor(),
+      shape: RoundedRectangleBorder(
+        side: _getBorder(),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      padding: const EdgeInsets.all(8),
       onPressed: onPressed,
-      child: Text(text),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          if (icon != null) ...[
+            Icon(icon, color: _getContentColor()),
+            const SizedBox(width: 8),
+          ],
+          Text(
+            text,
+            style: TextStyle(color: _getContentColor()),
+          ),
+        ],
+      ),
     );
+  }
+
+  Color _getBackgroundColor() {
+    switch (variant) {
+      case DSButtonVariant.primary:
+        return DSColors.primary;
+      case DSButtonVariant.secondary:
+        return DSColors.secondary;
+      case DSButtonVariant.tertiary:
+        return Colors.white;
+    }
+  }
+
+  Color _getContentColor() {
+    switch (variant) {
+      case DSButtonVariant.primary:
+        return Colors.white;
+      case DSButtonVariant.secondary:
+        return Colors.white;
+      case DSButtonVariant.tertiary:
+        return DSColors.secondary;
+    }
+  }
+
+  BorderSide _getBorder() {
+    switch (variant) {
+      case DSButtonVariant.primary:
+        return BorderSide.none;
+      case DSButtonVariant.secondary:
+        return BorderSide.none;
+      case DSButtonVariant.tertiary:
+        return const BorderSide(
+          color: DSColors.secondary,
+        );
+    }
   }
 }
