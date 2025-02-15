ac9be43a70729cee5298dbca135f5dbd0d1d5299
Luis Ortiz <luisortramirez2211@gmail.com>
2025-02-14 23:23:04 -0600

    ci: adding github action for widgetbook

--- widgetbook/lib/components/widgets/button.dart
+++ widgetbook/lib/components/widgets/button.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_design_system/components/components.dart';
+import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
 
 @widgetbook.UseCase(name: 'Primary', type: DSButton)
 Widget primaryButton(BuildContext context) {
   return DSButton(
-    text: 'Primary',
+    text: context.knobs.string(
+      label: '',
+    ),
     onPressed: () {},
   );
 }
